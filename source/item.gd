0b9a3ff3db9682356cd40bf0700c84bdb3a267bc
Scforever-Shawn <49487178+Scforever-Shawn@users.noreply.github.com>
2026-09-09 01:05:48 +0800

    feat: synchronize network item lifecycle

--- source/item.gd
+++ source/item.gd
@@ -2,13 +2,29 @@ extends Area2D
 class_name Item
 
 func _ready():
-    # Connect body entered signal
     body_entered.connect(_on_body_entered)
-    # Destroy after 10 seconds if not collected
-    get_tree().create_timer(10.0).timeout.connect(queue_free)
+    get_tree().create_timer(10.0).timeout.connect(_on_expire)
 
 func _on_body_entered(body):
     if body is Ball:
-        AudioManager.play_sound("powerup")
-        body.trigger_power_up()
+        var scene = get_tree().current_scene
+        if scene.has_method("is_network_game") and scene.is_network_game():
+            if not scene.has_method("is_host") or not scene.is_host():
+                return
+            AudioManager.play_sound("powerup")
+            body.trigger_power_up()
+            scene.consume_network_item(self)
+        else:
+            AudioManager.play_sound("powerup")
+            body.trigger_power_up()
+            queue_free()
+
+func _on_expire():
+    if not is_inside_tree():
+        return
+    var scene = get_tree().current_scene
+    if scene.has_method("is_network_game") and scene.is_network_game():
+        if scene.has_method("is_host") and scene.is_host():
+            scene.consume_network_item(self)
+    else:
         queue_free()
